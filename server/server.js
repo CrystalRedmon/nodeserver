9bfc68c455666909f722b0a606b2fee371092b04
Crystal Redmon <cato2848@gmail.com>
2023-01-23 20:10:22 -0600

    add bodyParser and post for /quotes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// bodyParser decodes the http request from the client
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // use require to use modules
-const quoteList = require('./public/modules/quoteList')
+const quoteList = require('./public/modules/quoteList');
+
+
 
 
 // express static file serving - public is the folder name
@@ -20,9 +26,18 @@ app.get('/quotes', (req, res) => {
     res.send(quoteList);
 })
 
+app.post('/quotes', (req, res)=>{
+
+    quoteList.push(req.body);
+    res.sendStatus(200);
+    console.log('this is the req.body: ', req.body );
+    
+})
+
 
 
 // Start up our server
 app.listen(port, () => {
   console.log('listening on port', port);
 });
+
