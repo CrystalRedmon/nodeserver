b816b7e961fe5a06c9d995036b367c4ebd77bdc8
Crystal Redmon <cato2848@gmail.com>
2023-01-24 19:17:16 -0600

    create quotes router

---
+++ server/routes/quotes.router.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let router = express.Router();
+let quotes = require('../public/modules/quoteList');
+
+
+
+
+router.get('/', (req, res) => {
+
+    console.log('request for /quotes was made');
+    res.status(200);
+    res.send(quotes);
+})
+
+router.post('/', (req, res)=>{
+    let quote = req.body
+    console.log('this is the req.body: ', quote);
+    quotes.push(quote);
+    res.send({message: 'Successfully added quote'});
+    
+    
+})
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,36 +3,20 @@ const express = require('express');
 // Create an instance of express by calling the function returned above - gives us an object
 const app = express();
 const port = 5000;
+let quotesRouter = require('../server/routes/quotes.router');
+
+app.use('/quotes', quotesRouter);
+
 
 // bodyParser decodes the http request from the client
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// use require to use modules
-const quoteList = require('./public/modules/quoteList');
 
 
 
 
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
-
-
-
-app.get('/quotes', (req, res) => {
-
-    console.log('request for /quotes was made');
-    res.status(200);
-    res.send(quoteList);
-})
-
-app.post('/quotes', (req, res)=>{
-
-    quoteList.push(req.body);
-    res.sendStatus(200);
-    console.log('this is the req.body: ', req.body );
-    
-})
 
 
 
