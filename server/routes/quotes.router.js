0a1b7ec88e02ae34e6dc8fd42a280d3febbdb197
Crystal Redmon <cato2848@gmail.com>
2023-01-25 15:18:44 -0600

    Add app.use(bodyParser.json(), remove use of express.router

--- server/routes/quotes.router.js
+++ server/routes/quotes.router.js
@@ -1,26 +1,26 @@
-let express = require('express');
-let router = express.Router();
-let quotes = require('../public/modules/quoteList');
+// let express = require('express');
+// let router = express.Router();
+// let quotes = require('../public/modules/quoteList');
 
 
 
 
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
 
-    console.log('request for /quotes was made');
-    res.status(200);
-    res.send(quotes);
-})
+//     console.log('request for /quotes was made');
+//     res.status(200);
+//     res.send(quotes);
+// })
 
-router.post('/', (req, res)=>{
-    let quote = req.body
-    console.log('this is the req.body: ', quote);
-    quotes.push(quote);
-    res.send({message: 'Successfully added quote'});
+// router.post('/', (req, res)=>{
+//     let quote = req.body
+//     console.log('this is the req.body: ', quote);
+//     quotes.push(quote);
+//     res.send({message: 'Successfully added quote'});
     
     
-})
+// })
 
 
 
-module.exports = router;
+// module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,20 +3,41 @@ const express = require('express');
 // Create an instance of express by calling the function returned above - gives us an object
 const app = express();
 const port = 5000;
+const quotes = require('./public/modules/quoteList')
+
 let quotesRouter = require('../server/routes/quotes.router');
-
-app.use('/quotes', quotesRouter);
-
-
 // bodyParser decodes the http request from the client
 let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+// app.use('/quotes', quotesRouter);
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
+
+
+
+
+app.get('/quotes', (req, res) => {
+
+    console.log('request for /quotes was made');
+    res.status(200);
+    res.send(quotes);
+})
+
+app.post('/quotes', (req, res)=>{
+    let quote = req.body
+    console.log('this is the req.body: ', quote);
+    quotes.push(quote);
+    res.send({message: 'Successfully added quote'});
+    
+    
+})
+
+
+
+
 
 
 
