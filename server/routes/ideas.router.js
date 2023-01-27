1e6045a0c124d4fa5eebd5ff095a3bda5e51df3b
Crystal Redmon <cato2848@gmail.com>
2023-01-26 19:00:42 -0600

    add get/byPrice endpoint to ideas router

--- server/routes/ideas.router.js
+++ server/routes/ideas.router.js
@@ -1,11 +1,40 @@
 let express = require('express');
 let app = express();
 let router = express.Router();
+const axios = require('axios');
 
 
 
 
+// get random activity
+router.get('/', (req, res) => {
 
+    axios.get('http://www.boredapi.com/api/activity/')
+    .then((response) => {
+        res.send(response.data.activity);
+    }).catch(err => {
+        res.sendStatus(500);
+        console.log('GET ideas failed: ', err);
+    });
+
+
+    console.log('I\'m going to get all of the ideas!');
+})
+
+
+router.get('/byPrice', (req, res) => {
+
+    axios.get(`http://www.boredapi.com/api/activity?minprice=.3&maxprice=1.0`)
+    .then((response) => {
+        res.send(response.data.activity);
+    }).catch(err => {
+        res.sendStatus(500);
+        console.log('GET ideas failed: ', err);
+    });
+
+
+    console.log('Make it cheap');
+})
 
 
 
