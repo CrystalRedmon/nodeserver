f9bbfc2cbac64d501d4eca22cb6777c699be1e78
Crystal Redmon <cato2848@gmail.com>
2023-01-31 06:03:50 -0600

    add select dropdown to select activity type

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import axios, * as others from 'axios';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
 
-const [activity, setActivity]=useState('');
+  const [activity, setActivity] = useState('');
+
+  const [type, setType] = useState('');
 
 
   const handleOnClick = () => {
     console.log('inside handleOnClick');
-   
+
     axios.get('/ideas')
       .then(response => {
         console.log('this is the response: ', response.data);
@@ -20,6 +22,16 @@ const [activity, setActivity]=useState('');
       })
 
   }
+
+  const getActivityByCriteria = (evt) => {
+    evt.preventDefault();
+
+    console.log('Getting Activity', type);
+
+
+  }
+
+
 
 
 
@@ -37,8 +49,31 @@ const [activity, setActivity]=useState('');
           Select Random Activity
         </button>
 
+        <br></br>
+        <br></br>
+
+        <form onSubmit={getActivityByCriteria}>
+          <label>Search For: </label>
+          <select onChange={evt => setType(evt.target.value)} name='type' id='type'>
+            <option value=''>Please Choose An Activity Type</option>
+            <option value='busywork'>Busywork</option>
+            <option value='charity'>Charity</option>
+            <option value='cooking'>Cooking</option>
+            <option value='diy'>DIY</option>
+            <option value='education'>Education</option>
+            <option value='music'>Music</option>
+            <option value='recreational'>Recreational</option>
+            <option value='relaxation'>Relaxation</option>
+            <option value='social'>Social</option>
+          </select>
+          <button type='submit'>
+            Get Activity
+          </button >
+        </form>
+
+
         <div>
-         {activity ? <p>{activity}</p> : <p>Results Go Here</p>}
+          {activity ? <p>{activity}</p> : <p>Results Go Here</p>}
         </div>
 
       </main>
